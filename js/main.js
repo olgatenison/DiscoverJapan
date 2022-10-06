39c9bf1c870a86122a2f232ca6e8336b14bec773
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-10-07 00:33:17 +0300

    add burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// header decoration bg
 (function () {
   const header = document.querySelector(".header");
   window.onscroll = () => {
@@ -7,4 +8,17 @@
       header.classList.remove("header_active");
     }
   };
+})();
+
+// burger handler
+(function () {
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector(".header__nav");
+  const menuCloseItem = document.querySelector(".header__nav-close");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header__nav_active");
+  });
+  menuCloseItem.addEventListener("click", () => {
+    menu.classList.remove("header__nav_active");
+  });
 })();
