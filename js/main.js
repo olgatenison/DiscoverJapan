797255fc3d144c7da64a887b50fa0367de05110d
olgatenison <88651744+olgatenison@users.noreply.github.com>
2022-10-08 21:56:31 +0300

    optimizate pic

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@
   const burgerItem = document.querySelector(".burger");
   const menu = document.querySelector(".header__nav");
   const menuCloseItem = document.querySelector(".header__nav-close");
+  const menuLinks = document.querySelectorAll(".header__link");
 
   burgerItem.addEventListener("click", () => {
     menu.classList.add("header__nav_active");
@@ -23,6 +24,13 @@
   menuCloseItem.addEventListener("click", () => {
     menu.classList.remove("header__nav_active");
   });
+  if (window.innerWidth < 768) {
+    for (let i = 0; i <= menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener("click", () => {
+        menu.classList.remove("header__nav_active");
+      });
+    }
+  }
 })();
 
 // Scroll to anchors
